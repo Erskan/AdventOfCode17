a8a6f894abf6cbac426823fdb46edd099949b5c6
Erik Mattsson Mårn <erik.mattssonmarn@consid.se>
2017-12-15 12:22:29 +0100

    Solution to day 15 generator duel

---
+++ day 15/generatorduel/generatorduel/Generator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace generatorduel
+{
+    public class Generator
+    {
+        public ulong CurrentValue { get; set; }
+        public ulong Factor { get; set; }
+        public const ulong Divisior = 2147483647;
+        public Generator(ulong seed, ulong factor)
+        {
+            Factor = factor;
+            CurrentValue = (seed * Factor) % Divisior;
+        }
+
+        public ulong Generate()
+        {
+            CurrentValue = (CurrentValue * Factor) % Divisior;
+            return CurrentValue;
+        }
+    }
+}
---
+++ day 15/generatorduel/generatorduel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace generatorduel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generator genA = new Generator(722, 16807);
+            Generator genB = new Generator(354, 48271);
+            uint matches = 0;
+
+            if (Match16Bits(genA.CurrentValue, genB.CurrentValue))
+                ++matches;
+
+            for (uint idx = 0; idx < 40000000; ++idx)
+            {
+                genA.Generate();
+                genB.Generate();
+
+                if (Match16Bits(genA.CurrentValue, genB.CurrentValue))
+                    ++matches;
+            }
+
+            Console.WriteLine($"Total matches: {matches}");
+            Console.ReadKey();
+        }
+
+        static bool Match16Bits(ulong valueA, ulong valueB)
+        {
+            ushort a = unchecked((ushort) valueA);
+            ushort b = unchecked((ushort) valueB);
+            return a == b;
+        }
+    }
+}
